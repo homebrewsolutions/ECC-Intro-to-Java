589d7a375f5c301c3da0692772a96d09467a41ba
homebrewsolutions <d3v1n0ndaxb0x@gmail.com>
2026-02-09 12:23:12 -0500

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,12 +1,13 @@
 package hello;
-import java.util.Scanner;
+import java.util.Scanner; // Importing scanner method
 public class EvenOdd {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
-        scanner.close();
-        
+        Scanner scanner = new Scanner(System.in); // Initizaling scanner
+        System.out.print("Enter a number: "); 
+        int num = scanner.nextInt(); // Storing next interger from the user as interger 'num'
+        scanner.close(); // Closing the scanner method
+
+        // Conditional Statemnent checking to see if the number input by user is even or odd 
         if (num % 2 == 0) {
             System.out.println(num + " is even.");
         } else {
@@ -14,3 +15,4 @@ public class EvenOdd {
         }
     }
 }
+
